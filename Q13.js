d01f11da100fc24bada7528b8338f5d1625430ae
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 20:57:22 +0500

    Add files via upload

---
+++ Q13.js
@@ -0,0 +1,6 @@
+"use strict";
+//  13:Your Own Array:Think of your favorite mode of transportation,such as a motorcycle or a car, and make a list that store several examples.Use your list to print a series of statements about these items,such as “I would like to own a Honda motorcycle"
+let transports = ["Mercedes", "Audi", "Honda motorcycle", "Ford Mustang"];
+console.log("I would like to own a", transports[0]);
+console.log("I would like to own a", transports[1]);
+console.log("I would like to own a", transports[3]);
